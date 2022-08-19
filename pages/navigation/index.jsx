f17d540ccbbb18d4c764e4ca4f3a41f5757e778c
sedhha <activity.schoolsh2@gmail.com>
2022-08-19 14:20:50 +0530

    Added Router replace

--- pages/navigation/index.jsx
+++ pages/navigation/index.jsx
@@ -6,6 +6,9 @@ export default function Navigation() {
   const router = useRouter();
   const onClickHandler = () =>
     randomPage.length > 0 && router.push(`navigation/${randomPage}`);
+
+  const onClickHandlerReplace = () =>
+    randomPage.length > 0 && router.replace(`navigation/${randomPage}`);
   return (
     <div className='p-4'>
       <h1>Router Navigation</h1>
@@ -18,8 +21,13 @@ export default function Navigation() {
       <br />
       <button
         onClick={onClickHandler}
-        className='p-2 mt-2 text-white bg-blue-600'>
+        className='p-2 mt-2 ml-2 text-white bg-blue-600'>
         Go to Specific Route
+      </button>
+      <button
+        onClick={onClickHandlerReplace}
+        className='p-2 mt-2 ml-2 text-white bg-blue-600'>
+        Go to Specific Route with Replace
       </button>
     </div>
   );
