d6622d75cc0c3a68718126d3e02be2328144d677
sedhha <activity.schoolsh2@gmail.com>
2022-08-19 13:27:50 +0530

    router push, reload, back

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 export default function SlugPage() {
   const router = useRouter();
   return <div>Blog Slug: {router.query.slug}</div>;
---
+++ pages/navigation/index.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Navigation() {
+  const [randomPage, setRandomPage] = useState('');
+  const router = useRouter();
+  const onClickHandler = () =>
+    randomPage.length > 0 && router.push(`navigation/${randomPage}`);
+  return (
+    <div className='p-4'>
+      <h1>Router Navigation</h1>
+      <input
+        value={randomPage}
+        onChange={(e) => setRandomPage(e.target.value)}
+        placeholder={'Go to Specific route'}
+        className='border'
+      />
+      <br />
+      <button
+        onClick={onClickHandler}
+        className='p-2 mt-2 text-white bg-blue-600'>
+        Go to Specific Route
+      </button>
+    </div>
+  );
+}
